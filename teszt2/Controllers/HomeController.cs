439b9c1c15f8ddb89ad1d3e13c31c1d4a6196839
Ericace03 <ericacebmf@gmail.com>
2024-05-06 11:13:59 +0200

    added repo

---
+++ teszt2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using teszt2.Models;
+
+
+namespace teszt2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Counter(CounterModel model)
+    {
+        ViewBag.Message = model.calculate();
+        return View("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ teszt2/Models/CounterModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using teszt2.Controllers;
+
+namespace teszt2.Models
+{
+	public class CounterModel
+	{
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public string operation { get; set; }
+
+        public string calculate()
+        {
+            int result = 0;
+            string message = "";
+
+            switch (operation)
+            {
+                case "add":
+                    result = number1 + number2;
+                    message = $"A két szám összege: {result}";
+                    break;
+                case "subtract":
+                    result = number1 - number2;
+                    message = $"A két szám különbsége: {result}";
+                    break;
+                case "multiply":
+                    result = number1 * number2;
+                    message = $"A két szám szorzata: {result}";
+                    break;
+                case "divide":
+                    if (number2 != 0)
+                    {
+                        result = number1 / number2;
+                        message = $"A két szám hányadosa: {result}";
+                    }
+                    else
+                    {
+                        message = "Nullával nem lehet osztani!";
+                    }
+                    break;
+                default:
+                    message = "Érvénytelen művelet!";
+                    break;
+            }
+        return message;
+        }
+	}
+}
+
